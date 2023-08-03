f2717f12840e917dc3bfc65b1d042b1195b61c91
Curtis Vogt <curtis.vogt@gmail.com>
2023-08-03 15:46:41 -0500

    Implement Language/WorkerType enums

--- src/wrappers/any.jl
+++ src/wrappers/any.jl
@@ -1,4 +1,5 @@
 # export ray_core_worker_julia
+export Language, WorkerType
 
 using CxxWrap
 using libcxxwrap_julia_jll
@@ -34,3 +35,65 @@ function node_manager_port()
 end
 
 initialize_coreworker() = initialize_coreworker(node_manager_port())
+
+# function Base.Symbol(language::Language)
+#     return if language === PYTHON
+#         :PYTHON
+#     elseif field === JAVA
+#         :JAVA
+#     elseif field === CPP
+#         :CPP
+#     elseif field === JULIA
+#         :JULIA
+#     else
+#         throw(ArgumentError("Unknown language: $language"))
+#     end
+# end
+
+# Base.instances(::Type{Language}) = (PYTHON, JAVA, CPP, JULIA)
+
+function Base.getproperty(::Type{Language}, field::Symbol)
+    return if field === :PYTHON
+        PYTHON
+    elseif field === :JAVA
+        JAVA
+    elseif field === :CPP
+        CPP
+    # elseif field === :JULIA
+    #     JULIA
+    else
+        Base.getfield(Language, field)
+    end
+end
+
+function Base.propertynames(::Type{Language}, private::Bool=false)
+    public_properties = (:PYTHON, :JAVA, :CPP, #= :JULIA =#)
+    return if private
+        tuple(public_properties..., fieldnames(typeof(Language))...)
+    else
+        public_properties
+    end
+end
+
+function Base.getproperty(::Type{WorkerType}, field::Symbol)
+    return if field === :WORKER
+        WORKER
+    elseif field === :DRIVER
+        DRIVER
+    elseif field === :SPILL_WORKER
+        SPILL_WORKER
+    elseif field === :RESTORE_WORKER
+        RESTORE_WORKER
+    else
+        Base.getfield(WorkerType, field)
+    end
+end
+
+function Base.propertynames(::Type{WorkerType}, private::Bool=false)
+    public_properties = (:WORKER, :DRIVER, :SPILL_WORKER, :RESTORE_WORKER)
+    return if private
+        tuple(public_properties..., fieldnames(typeof(WorkerType))...)
+    else
+        public_properties
+    end
+end
