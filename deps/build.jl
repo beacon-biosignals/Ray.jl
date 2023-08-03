a7969f651622a336fe6a7a673f26955bc0a8b8eb
Glenn Moynihan <glennmoy@gmail.com>
2023-08-03 15:44:58 +0100

    Update deps/build.jl
    Co-authored-by: Curtis Vogt <curtis.vogt@gmail.com>

--- deps/build.jl
+++ deps/build.jl
@@ -34,8 +34,7 @@ end
 
 # Add entry to depot specific Overrides.toml
 overrides_toml = joinpath(first(DEPOT_PATH), "artifacts", "Overrides.toml")
-!isfile(overrides_toml) && touch(overrides_toml)
-overrides_dict = TOML.parsefile(overrides_toml)
+overrides_dict = isfile(overrides_toml) ? TOML.parsefile(overrides_toml) : Dict{String,Any}()
 overrides_dict[pkg_uuid] = Dict("ray_core_worker_julia" => abspath(artifact_dir))
 open(overrides_toml, "w") do io
     TOML.print(io, overrides_dict)
