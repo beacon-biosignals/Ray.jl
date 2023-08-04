2200dcf1da8c59cc79de83265ccaff6b8554f2b0
Curtis Vogt <curtis.vogt@gmail.com>
2023-08-04 11:57:36 -0500

    Install Ray CLI globally

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,23 +1,10 @@
 using ray_core_worker_julia_jll: initialize_coreworker, shutdown_coreworker, put, get
 using Test
 
-spawn_head = !success(`ray status`)
-if spawn_head
-    @info "Starting local head node"
-    run(pipeline(`ray start --head`, stdout=devnull))
-end
-
-initialize_coreworker()
-
 @testset "ray_core_worker_julia_jll.jl" begin
-    try
-        obj_ref1 = put("Greetings from Julia!")
-        obj_ref2 = put("Take me to your leader")
-
-        @test get(obj_ref2) == "Take me to your leader"
-        @test get(obj_ref1) == "Greetings from Julia!"
-    finally
-        shutdown_coreworker()
-        spawn_head && run(pipeline(`ray stop`, stdout=devnull))
+    setup_ray_head_node() do
+        setup_core_worker() do
+            include("put_get.jl")
+        end
     end
 end
