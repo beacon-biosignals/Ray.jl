532e7e27dae4ae1bbc09e9b14fc77e92d07fdf9a
Curtis Vogt <curtis.vogt@gmail.com>
2023-08-04 15:26:35 -0500

    Remove demo

--- demo.jl
+++
@@ -1,8 +0,0 @@
-using ray_core_worker_julia_jll: initialize_coreworker, shutdown_coreworker, put, get
-
-initialize_coreworker()
-obj_id = put("Greetings from Julia!")
-result = get(obj_id)
-shutdown_coreworker()
-
-@show result
