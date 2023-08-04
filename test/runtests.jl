aa934b5466ee7c316a5e8debc8fef6d21777aae2
Curtis Vogt <curtis.vogt@gmail.com>
2023-08-04 12:46:06 -0500

    Include utils.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,8 @@
 using ray_core_worker_julia_jll: initialize_coreworker, shutdown_coreworker, put, get
 using Test
 
+include("utils.jl")
+
 @testset "ray_core_worker_julia_jll.jl" begin
     setup_ray_head_node() do
         setup_core_worker() do
