9ab4470d83215e8efb894aa216b036096c258c83
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2023-08-10 14:05:19 -0400

    make aqua happy

--- Ray.jl/src/Ray.jl
+++ Ray.jl/src/Ray.jl
@@ -1,9 +1,11 @@
 """
     Ray
 
-This package TODO
+This package provides user-facing interface for Julia-on-Ray.
 """
 module Ray
+
+using ray_core_worker_julia_jll
 
 # Write your package code here.
 
