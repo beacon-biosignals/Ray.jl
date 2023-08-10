821999235caecae96a149370110ad441253883b0
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2023-08-10 18:53:21 -0400

    bye bye

--- Ray.jl/test/runtests.jl
+++ Ray.jl/test/runtests.jl
@@ -1,9 +1,13 @@
-include("set_up_tests.jl")
+using Test
+using Ray
+using Aqua
 
 @testset "Ray.jl" begin
     @testset "Aqua" begin
         Aqua.test_all(Ray; ambiguities=false)
     end
+
+    
 
     # include additional test files here
 end
