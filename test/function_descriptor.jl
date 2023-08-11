ae91c3cf5c5055d24b1c6ac22a7501310b14b0f4
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2023-08-11 15:16:03 -0400

    better

--- test/function_descriptor.jl
+++ test/function_descriptor.jl
@@ -1,10 +1,12 @@
-fd = function_descriptor(isless)
+@tepstset "function descriptor" begin
+    fd = function_descriptor(isless)
 
-@test fd isa JuliaFunctionDescriptor
-@test fd.module_name == "Base"
-@test fd.function_name == "isless"
-# hash may not be consistent across versions/environments
-@test fd.function_hash isa AbstractString
-@test length(fd.function_hash) == 16
-# hash may not be consistent across versions/environments
-@test startswith(string(fd), "{type=JuliaFunctionDescriptor, module_name=Base, function_name=isless, function_hash="
+    @test fd isa JuliaFunctionDescriptor
+    @test fd.module_name == "Base"
+    @test fd.function_name == "isless"
+    # hash may not be consistent across versions/environments
+    @test fd.function_hash isa AbstractString
+    @test length(fd.function_hash) == 16
+    # hash may not be consistent across versions/environments
+    @test startswith(string(fd), "{type=JuliaFunctionDescriptor, module_name=Base, function_name=isless, function_hash=")
+end
