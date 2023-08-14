da07bbaf9afbfa1318285e089b2761114a14b017
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2023-08-14 17:27:20 -0400

    I crave teh sweet release of death

--- Ray.jl/src/function_manager.jl
+++ Ray.jl/src/function_manager.jl
@@ -39,13 +39,13 @@ function_key(fd::JuliaFunctionDescriptor, job_id) = string("RemoteFunction:", jo
 function export_function!(fm::FunctionManager, f, job_id)
     fd = function_descriptor(f)
     key = function_key(fd, job_id)
-
+    @show fd
     if Exists(fm.gcs_client, FUNCTION_MANAGER_NAMESPACE,
               deepcopy(key), # DFK: I _think_ the string memory may be freed if we don't copy
               -1)
-        @debug "function already present in GCS store:" key f
+        @debug "function already present in GCS store:" fd key f
     else
-        @debug "exporting function to GCS store:" key f
+        @debug "exporting function to GCS store:" fd key f
         val = base64encode(serialize, f)
         Put(fm.gcs_client, FUNCTION_MANAGER_NAMESPACE, key, val, true, -1)
     end
