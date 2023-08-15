6b58f801d4d228873de03739a2d100c5e6a61c2c
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2023-08-15 14:09:48 -0400

    does not need core worker

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,13 +12,13 @@ end
 
 @testset "ray_core_worker_julia_jll.jl" begin
     include("buffer.jl")
+    include("function_descriptor.jl")
 
     setup_ray_head_node() do
         # GCS client only needs head node
         include("gcs_client.jl")
         setup_core_worker() do
             include("put_get.jl")
-            include("function_descriptor.jl")
         end
     end
 end
