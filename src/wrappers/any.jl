89b83e77638a9ad8cda6ecb1867bc54dd7296cbb
Glenn Moynihan <glennmoy@gmail.com>
2023-08-17 16:10:07 +0100

    Use a C type then I guess?

--- src/wrappers/any.jl
+++ src/wrappers/any.jl
@@ -166,7 +166,7 @@ function start_worker(raylet_socket, store_socket, ray_address, node_ip_address,
                                           # ```
                                           # Using `ConstCxxRef` doesn't seem supported
                                           # (i.e. `const &`)
-                                          (RayFunctionAllocated, StdVectorAllocated))
+                                          (RayFunctionAllocated, std::vector))
 
     @info "cfunction generated!"
     return initialize_coreworker_worker(raylet_socket, store_socket,
