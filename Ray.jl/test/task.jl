b3503a3bfe014efbee27b1f64307daa43b47dc20
Glenn Moynihan <glennmoy@gmail.com>
2023-08-23 17:30:32 +0100

    make test use Uint8
    Co-authored-by: Curtis Vogt <curtis.vogt@gmail.com>

--- Ray.jl/test/task.jl
+++ Ray.jl/test/task.jl
@@ -1,12 +1,12 @@
 @testset "Submit task" begin
     oid1 = submit_task(length, "hello")
-    oid2 = submit_task(max, 1, 2)
+    oid2 = submit_task(max, 0x00, 0xff)
 
     result1 = deserialize(IOBuffer(take!(ray_core_worker_julia_jll.get(oid1))))
     @test result1 isa Int
     @test result1 == 5
 
     result2 = deserialize(IOBuffer(take!(ray_core_worker_julia_jll.get(oid2))))
-    @test result2 isa Int
-    @test result2 == 2
+    @test result2 isa UInt8
+    @test result2 == 0xff
 end
