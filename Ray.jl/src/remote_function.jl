2280c401532d35be27503b19868b33b82594a73c
Curtis Vogt <curtis.vogt@gmail.com>
2023-08-28 14:55:28 -0500

    Add comments

--- Ray.jl/src/remote_function.jl
+++ Ray.jl/src/remote_function.jl
@@ -1,3 +1,15 @@
+# The design of `flatten_args` and `recover_args` is heavily based upon the Python Ray
+# client which uses a similar approach
+# (https://github.com/ray-project/ray/blob/ray-2.5.1/python/ray/_private/signature.py#L81).
+# One area of concern is that the flattening occurs before Ray task arguments are determined
+# if they should be passed by value or by reference
+# (https://github.com/ray-project/ray/blob/ray-2.5.1/python/ray/_raylet.pyx#L695) which may
+# be problematic as you would only want to add the values to the object store and not the
+# associated keyword.
+
+# TODO: Currently we are completely ignoring cross language support
+# https://github.com/ray-project/ray/blob/ray-2.5.1/python/ray/remote_function.py#L379
+
 # Underscores are not allowed to be used as Julia function keywords
 # https://docs.julialang.org/en/v1/manual/variables/#man-allowed-variable-names
 const ARG_KEY = :_
