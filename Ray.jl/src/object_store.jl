5a9ddb08ee0adc3b45d8713be28b47da2e580b19
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2023-09-06 11:21:20 -0400

    polling `Ray.get`

--- Ray.jl/src/object_store.jl
+++ Ray.jl/src/object_store.jl
@@ -14,7 +14,7 @@ end
 put(obj_ref::ObjectRef) = obj_ref
 
 """
-    Ray.get(object_id::ObjectIDAllocated; timeout_ms=-1)
+    Ray.get(object_id::ObjectIDAllocated)
 
 Retrieves the data associated with the `object_id` from the object store.  This
 method is blocking until the data is available in the local object store, even
@@ -23,10 +23,20 @@ if run in an `@async` task.
 If the task that generated the `ObjectID` failed with a Julia exception, the
 captured exception will be thrown on `get`.
 """
-get(obj_ref::ObjectRef) = get(obj_ref.oid)
-get(oid::ray_jll.ObjectIDAllocated; timeout_ms::Int=-1) = get(ray_jll.get(oid, timeout_ms))
+get(obj_ref::ObjectRef; pollint_seconds=0.1) = get(obj_ref.oid; pollint_seconds)
+function get(oid::ray_jll.ObjectIDAllocated; pollint_seconds=0.1)
+    # we poll here with a 0 timeout in order to provide immediate return and
+    # create a yield point so this is `@async` friendly
+    timeout_ms = 0
+    result = get(ray_jll.get(oid, timeout_ms))
+    while result === nothing
+        sleep(pollint_seconds)
+        result = get(ray_jll.get(oid, timeout_ms))
+    end
+    return result
+end
 get(ray_obj::SharedPtr{ray_jll.RayObject}) = _get(take!(ray_jll.GetData(ray_obj[])))
-get(x) = x
+get(x; kwargs...) = x
 
 function _get(bytes)
     result = deserialize_from_bytes(bytes)
