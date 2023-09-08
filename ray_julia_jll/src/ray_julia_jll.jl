0ec2351dfb4e2259905ff0277d6962fb866594a7
Curtis Vogt <curtis.vogt@gmail.com>
2023-09-08 11:14:29 -0500

    Generate enum getproperty/propertyname functions

--- ray_julia_jll/src/ray_julia_jll.jl
+++ ray_julia_jll/src/ray_julia_jll.jl
@@ -1,9 +1,10 @@
-# Use baremodule to shave off a few KB from the serialized `.ji` file
-baremodule ray_julia_jll
+module ray_julia_jll
 using Base
 using Base: UUID
 import JLLWrappers
 using Pkg
+
+include("expr.jl")
 
 JLLWrappers.@generate_main_file_header("ray_julia")
 JLLWrappers.@generate_main_file("ray_julia", UUID("c348cde4-7f22-4730-83d8-6959fb7a17ba"))
