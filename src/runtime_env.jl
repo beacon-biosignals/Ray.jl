cd0bb30636cfd228a48f916457fcbf388052b9a5
Curtis Vogt <curtis.vogt@gmail.com>
2023-09-14 12:57:41 -0500

    Update to using julia_command in runtime_env

--- src/runtime_env.jl
+++ src/runtime_env.jl
@@ -27,7 +27,7 @@ function json_dict(runtime_env::RuntimeEnv)
 
     # The keys of `context` must match what is supported by the Python `RuntimeEnvContext`:
     # https://github.com/ray-project/ray/blob/ray-2.5.1/python/ray/_private/runtime_env/context.py#L20-L45
-    context = Dict("command" => cmd.exec, "env_vars" => env_vars)
+    context = Dict("julia_command" => cmd.exec, "env_vars" => env_vars)
 
     return context
 end
