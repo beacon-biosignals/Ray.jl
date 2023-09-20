99e64bf164029457b0484374879aa5085736c0af
Curtis Vogt <curtis.vogt@gmail.com>
2023-09-20 15:49:40 -0500

    Report task_id

--- src/exceptions.jl
+++ src/exceptions.jl
@@ -2,15 +2,16 @@ struct RayTaskException <: Exception
     task_name::String
     pid::Int
     ip::IPAddr
+    task_id::String
     captured::CapturedException
 end
 
 function RayTaskException(task_name::AbstractString, captured::CapturedException)
-    return RayTaskException(task_name, getpid(), getipaddr(), captured)
+    return RayTaskException(task_name, getpid(), getipaddr(), get_task_id(), captured)
 end
 
 function Base.showerror(io::IO, ex::RayTaskException, bt=nothing; backtrace=true)
-    print(io, "RayTaskException: $(ex.task_name) (pid=$(ex.pid), ip=$(ex.ip))")
+    print(io, "RayTaskException: $(ex.task_name) (pid=$(ex.pid), ip=$(ex.ip), task_id=$(ex.task_id))")
     if bt !== nothing && backtrace
         Base.show_backtrace(io, bt)
     end
