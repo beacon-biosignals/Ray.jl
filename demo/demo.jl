6d4b476946b47751c069583982448f1e087e3e84
Curtis Vogt <curtis.vogt@gmail.com>
2023-09-20 14:49:08 -0500

    Debugging information in demo.jl

--- demo/demo.jl
+++ demo/demo.jl
@@ -1,5 +1,19 @@
 using Ray
+import ray_julia_jll as ray_jll
+
+# Show `RAY_*` environmental variables
+for (k, v) in pairs(ENV)
+    if startswith(k, "RAY_")
+        println("$k=$(repr(v))")
+    end
+end
+println()
+
 Ray.init()
 
-return_ref = Ray.submit_task(max, (1, 2))
-@show Ray.get(return_ref)
+@show Ray.get_job_id()
+
+@show Ray.get(Ray.submit_task(max, (1, 2)))
+
+f = (x, y) -> max(x, y)
+@show Ray.get(Ray.submit_task(f, (3, 4)))
