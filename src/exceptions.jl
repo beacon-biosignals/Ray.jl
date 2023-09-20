74d19a87e0fdedbfed7b1a5708d74ee62b387f02
Curtis Vogt <curtis.vogt@gmail.com>
2023-09-20 15:49:40 -0500

    Show task ID in RayTaskException

--- src/exceptions.jl
+++ src/exceptions.jl
@@ -2,15 +2,16 @@ struct RayTaskException <: Exception
     task_name::String
     pid::Int
     ip::IPAddr
+    task_id::String
     captured::CapturedException
 end
 
 function RayTaskException(task_name::AbstractString, captured::CapturedException)
-    return RayTaskException(task_name, getpid(), getipaddr(), captured)
+    return RayTaskException(task_name, getpid(), getipaddr(), get_task_id(), captured)
 end
 
 function Base.showerror(io::IO, ex::RayTaskException, bt=nothing; backtrace=true)
-    print(io, "RayTaskException: $(ex.task_name) (pid=$(ex.pid), ip=$(ex.ip))")
+    print(io, "RayTaskException: $(ex.task_name) (pid=$(ex.pid), ip=$(ex.ip), task_id=$(ex.task_id))")
     if backtrace
         bt !== nothing && Base.show_backtrace(io, bt)
         println(io)
--- test/exceptions.jl
+++ test/exceptions.jl
@@ -1,12 +1,12 @@
 @testset "RayTaskException" begin
     @testset "showerror" begin
         e = ErrorException("foo")
-        e = Ray.RayTaskException("test2", 2, ip"127.0.0.1", CapturedException(e, backtrace()))
-        rte = Ray.RayTaskException("test1", 1, ip"127.0.0.1", CapturedException(e, backtrace()))
+        e = Ray.RayTaskException("test2", 2, ip"127.0.0.1", "t2", CapturedException(e, backtrace()))
+        rte = Ray.RayTaskException("test1", 1, ip"127.0.0.1", "t1", CapturedException(e, backtrace()))
 
         str = sprint(showerror, rte)
-        @test occursin(r"^\QRayTaskException: test1 (pid=1, ip=127.0.0.1)\E"m, str)
-        @test occursin(r"^\Qnested exception: RayTaskException: test2 (pid=2, ip=127.0.0.1)\E"m, str)
+        @test occursin(r"^\QRayTaskException: test1 (pid=1, ip=127.0.0.1, task_id=t1)\E"m, str)
+        @test occursin(r"^\Qnested exception: RayTaskException: test2 (pid=2, ip=127.0.0.1, task_id=t2)\E"m, str)
         @test occursin(r"^\Qnested exception: foo\E"m, str)
 
         structure_regex = r"""
@@ -24,8 +24,8 @@
         str = sprint(rte) do io, args...
             showerror(io, args...; backtrace=false)
         end
-        @test occursin(r"^\QRayTaskException: test1 (pid=1, ip=127.0.0.1)\E"m, str)
-        @test occursin(r"^\Qnested exception: RayTaskException: test2 (pid=2, ip=127.0.0.1)\E"m, str)
+        @test occursin(r"^\QRayTaskException: test1 (pid=1, ip=127.0.0.1, task_id=t1)\E"m, str)
+        @test occursin(r"^\Qnested exception: RayTaskException: test2 (pid=2, ip=127.0.0.1, task_id=t2)\E"m, str)
         @test occursin(r"^\Qnested exception: foo\E"m, str)
 
         structure_regex = r"""
