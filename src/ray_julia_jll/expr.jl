b911f630616c1d4f4e4332e82435a3ea453b9c08
Curtis Vogt <curtis.vogt@gmail.com>
2023-10-04 16:25:54 -0500

    Create _enum_symbol_accessor_expr

--- src/ray_julia_jll/expr.jl
+++ src/ray_julia_jll/expr.jl
@@ -15,6 +15,26 @@ function _enum_symbol_constructor_expr(base_type::Type, members)
     end
 end
 
+function _enum_symbol_accessor_expr(base_type::Type, members)
+    base_type_sym = nameof(base_type)
+    body = Expr(:if, :(member === $(members[1])), QuoteNode(members[1]), nothing)
+    ex = body
+    for member in members[2:end]
+        ex.args[3] = Expr(:elseif, :(member === $member), QuoteNode(member), nothing)
+        ex = ex.args[3]
+    end
+    ex.args[3] = :(throw(ArgumentError("$($base_type_sym) has no member named: $member")))
+
+    base_type_sym = nameof(base_type)
+    members_tuple = Expr(:tuple, members...)
+
+    return quote
+        function Base.Symbol(member::$base_type)
+            return $body
+        end
+    end
+end
+
 function _enum_instances_expr(base_type::Type, members)
     base_type_sym = nameof(base_type)
     members_tuple = Expr(:tuple, members...)
@@ -26,18 +46,3 @@ function _enum_instances_expr(base_type::Type, members)
     end
 end
 
-# function Base.Symbol(language::Language)
-#     return if language === PYTHON
-#         :PYTHON
-#     elseif field === JAVA
-#         :JAVA
-#     elseif field === CPP
-#         :CPP
-#     elseif field === JULIA
-#         :JULIA
-#     else
-#         throw(ArgumentError("Unknown language: $language"))
-#     end
-# end
-
-# Base.instances(::Type{Language}) = (PYTHON, JAVA, CPP, JULIA)
