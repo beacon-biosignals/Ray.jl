fc6a5a69dd178effff7cb70716fd36440ae8ee5e
Curtis Vogt <curtis.vogt@gmail.com>
2023-10-06 14:32:14 -0500

    fixup! Raise error when encountering metadata

--- src/ray_serializer.jl
+++ src/ray_serializer.jl
@@ -86,7 +86,7 @@ function deserialize_from_ray_object(ray_obj::SharedPtr{ray_jll.RayObject},
                                      outer_object_ref=nothing)
     metadata = ray_jll.get_metadata(ray_obj)
     if !isnothing(metadata)
-        from = isnothing(outer_object_ref) ? "" : "from `$(repr(outer_object_ref))`"
+        from = isnothing(outer_object_ref) ? "" : " from `$(repr(outer_object_ref))`"
         error("Encountered unhandled metadata$from: $(String(metadata))")
     end
 
