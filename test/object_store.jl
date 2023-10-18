e0f17ead1bc2c05b988d0bba23022a59f46bc054
Curtis Vogt <curtis.vogt@gmail.com>
2023-10-18 13:33:39 -0500

    Remove deepcopy owner address test

--- test/object_store.jl
+++ test/object_store.jl
@@ -59,24 +59,6 @@
         obj_ref = Ray.put(1)
         @test Ray.has_owner(obj_ref)
     end
-
-    @testset "deepcopy object reference owner address" begin
-        obj1 = Ray.put(42)
-        addr = Ray.get_owner_address(obj1)
-        obj2 = ObjectRef(Ray.hex_identifier(obj1), addr, "")
-        obj3 = deepcopy(obj2)
-
-        @test obj1.owner_address != addr  # Usually only populated upon deserialization
-        @test obj2.owner_address == addr
-        @test obj3.owner_address == addr
-
-        finalize(obj2)
-        yield()
-
-        # Avoid comparing against `addr` here as the finalizer could modify it in place
-        # allowing this test to pass.
-        @test obj3.owner_address == Ray.get_owner_address(obj1)
-    end
 end
 
 @testset "serialize_to_ray_object" begin
