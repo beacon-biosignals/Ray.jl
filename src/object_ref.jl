5f2274078e3b49869c83967eea75d7c5b0dd9578
Curtis Vogt <curtis.vogt@gmail.com>
2023-10-23 12:46:09 -0500

    Update to use CxxWrap 0.14.1 (#217)
    * Update to CxxWrap 0.14.1
    
    * Fix StdVector creation with empty vector
    
    * Minimal updates to safe_convert
    
    * Add more compat entries for build/Project.toml
    
    * Drop unnecessary Binary accessors

--- src/object_ref.jl
+++ src/object_ref.jl
@@ -119,8 +119,6 @@ function _register_ownership(obj_ref::ObjectRef, outer_obj_ref::Union{ObjectRef,
             ray_jll.Nil(ray_jll.ObjectID)
         end
 
-        serialized_object_status = safe_convert(StdString, serialized_object_status)
-
         # https://github.com/ray-project/ray/blob/ray-2.5.1/python/ray/_raylet.pyx#L3329
         # https://github.com/ray-project/ray/blob/ray-2.5.1/src/ray/core_worker/core_worker.h#L543
         ray_jll.RegisterOwnershipInfoAndResolveFuture(worker, obj_ref.oid, outer_object_id,
