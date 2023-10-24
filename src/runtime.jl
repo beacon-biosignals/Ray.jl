90589092b2d37dd77d529ab2e53a04782a454811
Glenn <glennmoy@gmail.com>
2023-10-24 17:46:36 +0100

    inline comment

--- src/runtime.jl
+++ src/runtime.jl
@@ -70,8 +70,7 @@ function init(runtime_env::Union{RuntimeEnv,Nothing}=nothing;
         runtime_env = JOB_RUNTIME_ENV[]
     end
 
-    # gcs-address=127.0.0.1:6379
-    gcs_address = read("/tmp/ray/ray_current_cluster", String)
+    gcs_address = read("/tmp/ray/ray_current_cluster", String) # host:port (e.g. 127.0.0.1:6379)
 
     opts = ray_jll.GcsClientOptions(gcs_address)
     GLOBAL_STATE_ACCESSOR[] = ray_jll.GlobalStateAccessor(opts)
