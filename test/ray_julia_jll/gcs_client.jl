c63f1f7b6579109fa273ff8a41ce2f4b19c88b1b
Curtis Vogt <curtis.vogt@gmail.com>
2023-10-25 23:22:39 -0500

    Test returned types JuliaGcsClient functions

--- test/ray_julia_jll/gcs_client.jl
+++ test/ray_julia_jll/gcs_client.jl
@@ -15,20 +15,33 @@
     @test_throws ErrorException Exists(client, ns, "computer")
 
     Connect(client) do client
-        @test Put(client, ns, "computer", "mistaek", false) == 1
-        @test Get(client, ns, "computer") == "mistaek"
-        @test Keys(client, ns, "") == ["computer"]
+        added = Put(client, ns, "computer", "mistaek", false)
+        @test added isa Bool
+        @test added == true
+
+        result = Get(client, ns, "computer")
+        @test result isa StdString
+        @test result == "mistaek"
+
+        results = Keys(client, ns, "")
+        @test results isa StdVector{StdString}
+        @test results == ["computer"]
+
         @test Keys(client, ns, "comp") == ["computer"]
         @test Keys(client, ns, "comppp") == []
-        @test Exists(client, ns, "computer")
+
+        exists = Exists(client, ns, "computer")
+        @test exists isa Bool
+        @test exists == true
 
         # no overwrite
-        @test Put(client, ns, "computer", "blah", false) == 0
+        @test !Put(client, ns, "computer", "blah", false)
         @test Get(client, ns, "computer") == "mistaek"
 
         # overwrite ("added" only increments on new key I think)
-        @test Put(client, ns, "computer", "blah", true) == 0
+        @test !Put(client, ns, "computer", "blah", true)
         @test Get(client, ns, "computer") == "blah"
+
 
         # throw on missing key
         @test_throws ErrorException Get(client, ns, "none")
